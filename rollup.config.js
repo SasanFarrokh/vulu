1297059025a3b679fab93658529633bf9477c680
Sasan Farrokh <s.farrokh@alibaba.ir>
2021-05-03 20:00:27 +0430

    chore: add rollup build

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: './src/index.ts',
+    output: [{
+        file: 'dist/index.js',
+        format: 'cjs',
+    }, {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+    }],
+    plugins: [
+        typescript()
+    ]
+};
